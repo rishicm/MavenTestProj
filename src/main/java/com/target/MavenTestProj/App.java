839427dbd85acd4efe9a1a13aa93c6d20d7ae147
rishicm <cmrishi@gmail.com>
2016-11-14 17:11:26 +0530

    Update App.java

--- src/main/java/com/target/MavenTestProj/App.java
+++ src/main/java/com/target/MavenTestProj/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
          System.out.println( "This is new file!" );
+         System.out.println( "This is new file11111!" );
     }
 }
